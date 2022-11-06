73c989b18dbdf1d66c07532a9f622412caf92f81
atrev5 <112439703+atrev5@users.noreply.github.com>
2022-11-05 22:25:51 -0500

    Display day and time

--- src/app.js
+++ src/app.js
@@ -1,17 +1,46 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${day} ${hour}:${minute}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.temperature.current
   );
   document.querySelector("#city").innerHTML = response.data.city;
-  document.querySelector("#description").innerHTML = response.data.condition.description;
+  document.querySelector("#description").innerHTML =
+    response.data.condition.description;
   document.querySelector("#humidity").innerHTML =
     response.data.temperature.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.time * 1000
+  );
 }
 
 let apiKey = "c143btabf6ob4a5faf5033a73eae142a";
-let query = "Chicago";
+let query = "Dallas";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
