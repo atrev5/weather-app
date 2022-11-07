00479b7b6046718b3ab3600a2567c51ebbe73ffc
atrev5 <112439703+atrev5@users.noreply.github.com>
2022-11-06 22:56:21 -0600

    Added unit conversions for °F and °C links

--- src/app.js
+++ src/app.js
@@ -22,10 +22,10 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
-  console.log(response.data.condition.icon_url);
+  fahrenheitTemperature = response.data.temperature.current;
+
   document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.temperature.current
+    fahrenheitTemperature
   );
   document.querySelector("#city").innerHTML = response.data.city;
   document.querySelector("#description").innerHTML =
@@ -41,27 +41,51 @@ function displayTemperature(response) {
   document
     .querySelector("#icon")
     .setAttribute("src", response.data.condition.icon_url);
-    document
-      .querySelector("#icon")
-      .setAttribute("alt", response.data.condition.description);
+  document
+    .querySelector("#icon")
+    .setAttribute("alt", response.data.condition.description);
 }
 
 function search(city) {
-    let apiKey = "c143btabf6ob4a5faf5033a73eae142a";
-   
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-    
-    axios.get(apiUrl).then(displayTemperature);
+  let apiKey = "c143btabf6ob4a5faf5033a73eae142a";
 
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-city");
-    search(searchInput.value);
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-city");
+  search(searchInput.value);
 }
 
-search("Dallas");
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.remove("active-link");
+  celsiusLink.classList.add("active-link");
+  let temperatureElement = document.querySelector("#temperature");
+  let celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active-link");
+  fahrenheitLink.classList.add("active-link");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitTemperature = null;
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+search("Dallas");
